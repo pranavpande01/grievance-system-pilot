9fae150a05919e03e1fec004dfdf636faf45a2c2
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-07 15:57:51 +0000

    first commit

--- tests/frontend.py
+++ tests/frontend.py
@@ -1,25 +1,51 @@
 import streamlit as st
 
-#########################################################
-if 'messages' not in  st.session_state:
-    st.session_state['messages']=[]
+MESSAGES_KEY = 'messages'
+APP_TITLE = "Portal Pilot"
 
-#########################################################
+def initialize_session_state():
+    """Initialize session state variables if they don't exist"""
+    if MESSAGES_KEY not in st.session_state:
+        st.session_state[MESSAGES_KEY] = []
 
-st.sidebar.title("portal pilot")
-if st.sidebar.button("new chat",disabled=not st.session_state['messages']):
-    st.session_state['messages']=[]
-    st.rerun()
+def clear_chat_history():
+    """Clear all messages from chat history"""
+    st.session_state[MESSAGES_KEY] = []
 
-if user_input:=st.chat_input():
-    st.session_state['messages'].append(
-        {
-            'role':'user',
-            'content':user_input
-        }
-    )
-    st.rerun()
-for i in st.session_state['messages']:
-    with st.chat_message(i['role']):
-        st.text(i['content'])
+def add_user_message(content):
+    """Add a user message to the chat history"""
+    message = {
+        'role': 'user',
+        'content': content
+    }
+    st.session_state[MESSAGES_KEY].append(message)
 
+def display_chat_messages():
+    """Display all messages in the chat history"""
+    for message in st.session_state[MESSAGES_KEY]:
+        with st.chat_message(message['role']):
+            st.write(message['content'])  
+
+def setup_sidebar():
+    """Setup the sidebar with controls"""
+    st.sidebar.title(APP_TITLE)
+    
+    if st.sidebar.button("new chat"):
+        clear_chat_history()
+        #st.rerun()
+
+def main():
+    """Main application logic"""
+
+    initialize_session_state()
+    setup_sidebar()
+    
+    user_input = st.chat_input("Type here...")
+    if user_input:
+        add_user_message(user_input)
+        #st.rerun()
+
+    display_chat_messages()
+
+if __name__ == "__main__":
+    main()
