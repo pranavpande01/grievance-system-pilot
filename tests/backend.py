a4a2dd25a4d6aab53a8615641e68c86898611385
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-07 16:22:16 +0000

    second commit

--- tests/backend.py
+++ tests/backend.py
@@ -3,7 +3,27 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, List
 from dotenv import load_dotenv
 
 load_dotenv()
+
+llm=ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
+
+class ChatState(TypedDict):
+    messages:Annotated[List[BaseMessage],add_messages]
+
+#define nodes
+def chat_node(state:ChatState):
+    return {
+        'message': llm.invoke(state['messages'])
+
+    }
+
+#define memory
+checkpointer=InMemorySaver()
+
+#define graph
+graph=StateGraph(ChatState)
+chatbot=graph.compile(checkpointer=checkpointer)
+
