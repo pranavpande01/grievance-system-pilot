974591558c78e1fa95d5b1295001f61ad9279d79
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-07 18:23:13 +0000

    second commit

--- tests/util.py
+++ tests/util.py
@@ -6,7 +6,7 @@ print("Backend is running... waiting for messages")
 
 while True:
     messages = r.xread({"chat_stream": "$"}, block=0, count=1)
-    print(messages)
+    #print(messages)
     for stream, events in messages:
         for msg_id, msg_data in events:
             print("User said:", msg_data["user_input"])
